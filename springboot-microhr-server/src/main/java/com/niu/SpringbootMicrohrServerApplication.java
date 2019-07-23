0e9d4e22ed3850b3252ad5963b4fb59857f0117f
18300603955 <oceaniu@163.com>
2019-07-23 16:06:58 +0800

    初始化数据库和实体类

--- springboot-microhr-server/src/main/java/com/niu/SpringbootMicrohrServerApplication.java
+++ springboot-microhr-server/src/main/java/com/niu/SpringbootMicrohrServerApplication.java
@@ -1,9 +1,13 @@
 package com.niu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@MapperScan("com.niu.mapper")
+@EnableCaching
 public class SpringbootMicrohrServerApplication {
 
 	public static void main(String[] args) {
