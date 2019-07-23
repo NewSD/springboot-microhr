bd4d4c22c9cd1e8ffa213f43ee39310a2b7fc859
18300603955 <oceaniu@163.com>
2019-07-23 15:21:34 +0800

    springboot-mocrohr-server init

---
+++ springboot-microhr-server/src/main/java/com/niu/springbootmicrohrserver/SpringbootMicrohrServerApplication.java
@@ -0,0 +1,13 @@
+package com.niu.springbootmicrohrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicrohrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicrohrServerApplication.class, args);
+	}
+
+}
